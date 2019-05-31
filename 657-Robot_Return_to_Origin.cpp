641896077b29283e0c08d3a89e0c1b271a1e912e
Tony Luo <tonyluolang@gmail.com>
2019-05-30 21:23:47 -0400

    657. Robot Return to Origin (Easy)

---
+++ 657-Robot_Return_to_Origin.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int ud = 0;
+        int lr = 0;
+        for (int i = 0; i < moves.size(); i++) {
+            if (moves[i] == 'U')
+                ud++;
+            else if (moves[i] == 'D')
+                ud--;
+            else if (moves[i] == 'L')
+                lr--;
+            else if (moves[i] == 'R')
+                lr++;
+        }
+        return ((ud == 0) && (lr == 0));
+    }
+};
