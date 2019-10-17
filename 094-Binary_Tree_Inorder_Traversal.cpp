9a7d982a653c0e8f2cb9a6abd09b484c06cf2197
Tony Luo <tonyluolang@gmail.com>
2019-10-16 23:28:30 -0400

    94. Binary Tree Inorder Traversal (Medium)

---
+++ 094-Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> s;
+        
+        while (root != NULL || !s.empty()) {
+            while (root != NULL) {
+                s.push(root);
+                root = root->left;
+            }
+            
+            root = s.top();
+            s.pop();
+            
+            result.push_back(root->val);
+            root = root->right;
+        }
+        
+        
+        return result;
+    }
+};
